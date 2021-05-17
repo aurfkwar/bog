72aa973337d5f115582097c5530d69fb28db8f71
aurfkwar <81349933+aurfkwar@users.noreply.github.com>
2021-05-16 19:34:31 -0500

    Update settings.py
    removed secret key

--- My_Block/My_Block/settings.py
+++ My_Block/My_Block/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0&d3g(kndwy#4ydl5w^n6eb#vg78y1qw%=st-&k7@i%c3t59w5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
